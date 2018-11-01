971a4d1584245c99591c0aba4440afb6180cfcb9
thegreatkingbear <pporri@yonsei.ac.kr>
2018-11-01 17:20:09 +0900

    update google analytics id

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -17,7 +17,7 @@ module.exports = {
   siteRssAuthor: "Casper User", // The author name used in the RSS file
   // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
   sitePaginationLimit: 10, // The max number of posts per page.
-  googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
+  googleAnalyticsID: "UA-128515948-1", // GA tracking ID.
   disqusShortname: "thegreatkingbearblog", // enables Disqus comments, visually deviates from original Casper theme.
   siteSocialUrls: [
     "https://github.com/thegreatkingbear/",
